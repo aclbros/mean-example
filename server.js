c8d6d508a9be12f10df40338bbe648a7a4b92a2b
Antonio Carretero <aclbros12@yahoo.es>
2016-07-12 08:30:30 +0000

    walking skeleton

---
+++ server.js
@@ -0,0 +1,57 @@
+var express = require("express"),
+    stylus = require("stylus"),
+    bodyParser = require("body-parser"),
+    logger = require("morgan"),
+    mongoose = require("mongoose");
+
+var env = process.env.NODE_ENV = process.env.NODE_ENV || "development";
+
+var app = express();
+
+function compile(str, path) {
+    return stylus(str).set('filename', path);
+}
+
+app.set('views', __dirname + '/server/views');
+app.set('view engine', 'jade');
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(stylus.middleware(
+    {
+        src: __dirname + '/public',
+        compile: compile
+    }
+));
+app.use(express.static(__dirname + '/public'));
+
+mongoose.connect('mongodb://user1:user1@ds017205.mlab.com:17205/mean-example');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error...'));
+db.on('open', function callback() {
+    console.log('multivision db opened');
+});
+var messageSchema = mongoose.Schema({ message: String });
+var Message = mongoose.model('Message', messageSchema);
+var mongoMessage;
+Message.findOne().exec(function(err, messageDoc) {
+    if (err) {
+        console.log('no results');
+    } else {
+        mongoMessage = messageDoc.message;
+    }
+});
+
+app.get('/partials/:partialPath', function(req, res) {
+    res.render('partials/' + req.params.partialPath);
+});
+
+app.get('*', function(req, res) {
+    res.render('index', {
+        mongoMessage: mongoMessage
+    });
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port);
+console.log("Listening on port " + port + "...");
